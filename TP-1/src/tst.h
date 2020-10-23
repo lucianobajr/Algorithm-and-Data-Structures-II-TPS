b57b879a300f1898242050ddfd36b5d7ed3525f9
BloodSymphony <70582558+BloodSymphony@users.noreply.github.com>
2020-10-23 18:34:16 -0300

    XD

--- TP-1/src/tst.h
+++ TP-1/src/tst.h
@@ -9,19 +9,20 @@
 typedef char data;
 typedef struct TSTNode *TSTNodePointer;
 
-typedef struct TSTNode {
+typedef struct TSTNode
+{
     data character;
     // Verdadeiro || 1 se este caractere for o último caractere de uma das palavras
     short isEndOfString : 1;
-    int contador;
-    TSTNodePointer left,eq,right;
-    
-} TSTNode;
+    TSTNodePointer left, eq, right;
 
+} TSTNode;
 
 void insert(TSTNodePointer *root, char *word);
 void printTSTUtil(TSTNodePointer root, char *buffer, int depth);
 void printTST(TSTNodePointer root);
 int searchTST(TSTNodePointer root, char *word);
+int counterWords(TSTNodePointer root,int *counter);
+void counterWordsUtil(TSTNodePointer root, int *counter, int depth);
 TSTNodePointer newNode(char data);
 #endif
